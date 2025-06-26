0a41a4952cbfab427f658410e14855a8ac550c78
Ghost_Protocol <das05swpna@gmail.com>
2025-06-26 19:45:16 +0530

    notes connected to mongodb

--- backend/routes/summaryRoutes.js
+++ backend/routes/summaryRoutes.js
@@ -2,6 +2,13 @@ const express=require('express');
 const multer = require("multer");
 const {summary, translation,quiz,mindMap
 }=require('../controllers/summaryController')
+const {getNotes,
+    getNote,
+    createNote,
+    deleteNote,
+    updateNote,
+    toggleFavorite
+}= require('../controllers/notesController')
 const router =express.Router()
 const upload = multer({ dest: "uploads/" });
 
@@ -10,6 +17,13 @@ router.post('/',summary)
 router.post('/translation', translation)
 router.post('/quiz', upload.single("pdf"), quiz)
 router.post("/mindmap", upload.single("file"), mindMap);
+
+router.get('/note',getNotes)
+router.get('/note/:id',getNote)
+router.post('/note',createNote)
+router.delete('/note/:id',deleteNote)
+router.patch('/note/:id',updateNote)
+router.patch('/note/:id/favorite', toggleFavorite); // ✅
 
 
 
